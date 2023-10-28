4c360952493aa670048ea3bad19e85d8f9bf7455
sachin.vashist <sachin.vashist@vodafone.com>
2023-10-29 02:32:30 +0530

    Onboarded CI-CD pipeline

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ResumeSite/',
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@assets": path.resolve(__dirname, "src/assets"),
-    },
-  },
-
 })
